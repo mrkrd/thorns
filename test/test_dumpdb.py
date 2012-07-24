161fe6b0046995592da8431f2f39690a73191dcd
Marek Rudnicki <marekrud@gmail.com>
2012-07-24 18:22:54 +0200

    marek@jade

--- test/test_dumpdb.py
+++ test/test_dumpdb.py
@@ -9,10 +9,7 @@ import shutil
 
 import numpy as np
 from numpy.testing import (
-    assert_array_equal,
-    assert_array_almost_equal,
-    assert_equal,
-    assert_almost_equal
+    assert_array_equal
 )
 
 from marlib import dumpdb
