db66cffcd15cbfc939e34f8d53d3ac000fea7ac6
Marek Rudnicki <marekrud@gmail.com>
2012-11-08 01:27:59 +0100

    simple dumpdb viewer

---
+++ viewdb.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from __future__ import division
+from __future__ import print_function
+
+__author__ = "Marek Rudnicki"
+
+import marlib as mr
+
+def main():
+    db = mr.loaddb()
+
+    print(db.to_string())
+
+
+if __name__ == "__main__":
+    main()
