e02c9baf8bb21f6fb1490cd7ff75dc9c26df4170
Marek Rudnicki <marekrud@gmail.com>
2014-06-26 15:11:55 +0200

    add bisection find_zero

--- thorns/util/__init__.py
+++ thorns/util/__init__.py
@@ -2,17 +2,17 @@
 
 """
 
+from __future__ import division, print_function, absolute_import
+
 from thorns.util.dumpdb import (
     dumpdb,
     loaddb,
     get_store,
 )
 
-from thorns.util.maps import (
-    map,
-    apply,
-)
+from thorns.util.maps import map, apply
 
+from . bisection import find_zero
 
 def mkdir(dirname):
     if not os.path.exists(dirname):
