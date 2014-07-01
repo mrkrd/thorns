aefcafd9a92602320fc7f855718f57048d5d5d9f
Marek Rudnicki <marekrud@gmail.com>
2014-07-01 23:51:10 +0200

    sphinx mock

--- docs/conf.py
+++ docs/conf.py
@@ -259,3 +259,29 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['pandas', 'numpy', 'scipy.signal', 'scipy']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
