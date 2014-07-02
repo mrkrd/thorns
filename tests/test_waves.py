3c77cf39577b1acf11f83c17246499a0dc120148
Marek Rudnicki <marekrud@gmail.com>
2014-07-02 21:49:35 +0200

    generate_electrical_amplitudes docstring and test

---
+++ tests/test_waves.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test thorns.waves module.
+
+"""
+
+from __future__ import division, absolute_import, print_function
+
+__author__ = "Marek Rudnicki"
+
+import numpy as np
+from numpy.testing import assert_equal
+
+import thorns.waves as wv
+
+
+def test_generate_electrical_amplitudes_2():
+
+    durations = [1, 0.5]
+
+    amplitudes = wv.generate_electrical_amplitudes(
+        durations=durations,
+        polarity=1,
+    )
+
+
+    assert_equal(amplitudes, [0.5, -1])
+
+
+def test_generate_electrical_amplitudes_3():
+
+    durations = [0.5, 1, 0.5]
+    ratio = 0.3
+    polarity = 1
+
+    amplitudes = wv.generate_electrical_amplitudes(
+        durations=durations,
+        polarity=polarity,
+        ratio=ratio
+    )
+
+    assert_equal(amplitudes, [0.3, -0.5, 0.7])
