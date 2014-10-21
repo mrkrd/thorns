e9119b8da335f62c1f9e7fcb5c0b3e233a457389
Marek Rudnicki <marekrud@gmail.com>
2014-10-21 19:45:42 +0200

    make_trains: better handling of empty trains

--- thorns/spikes.py
+++ thorns/spikes.py
@@ -92,6 +92,9 @@ def make_trains(data, **kwargs):
         spikes = [spks for spks in data.spiketimes.itervalues()]
         trains = _arrays_to_trains(spikes, **meta)
 
+    elif len(data) == 0:
+        trains = pd.DataFrame(columns=(['spikes'] + meta.keys()))
+
     elif isinstance(data[0], Iterable):
         trains = _arrays_to_trains(data, **meta)
 
@@ -277,7 +280,7 @@ def trim(spike_trains, start=0, stop=None):
 
 
     durations = np.array(spike_trains['duration'])
-    durations[ durations>tmaxs ] = tmaxs[ durations>tmaxs ]
+    durations[ np.array(durations>tmaxs) ] = tmaxs[ np.array(durations>tmaxs) ]
     durations -= tmin
 
 
