8f5aeec0d2eb01d801b6ba92695f82ace20b6c3f
Marek Rudnicki <marekrud@gmail.com>
2015-02-12 15:55:56 +0100

    better map demo

--- examples/map_demo.py
+++ examples/map_demo.py
@@ -8,7 +8,7 @@ from __future__ import division, print_function, absolute_import
 from __future__ import unicode_literals
 
 __author__ = "Marek Rudnicki"
-__copyright__ = "Copyright 2014, Marek Rudnicki"
+__copyright__ = "Copyright 2014-2015, Marek Rudnicki"
 __license__ = "GPLv3+"
 
 
@@ -18,22 +18,32 @@ import pandas as pd
 import thorns as th
 
 
-def square(x):
-    y = {'y': x**2}
+def multiply(a, b):
+    """Multiply two numbers."""
+
+    # The output should be wrapped in a dict
+    y = {'y': a * b}
+
     return y
 
 
 def main():
 
-    xs = {'x': np.arange(10)}
+    # Keys in the input dict MUST correspond to the key word
+    # arguments of the function (e.g. multiply)
+    xs = {
+        'a': np.arange(3),
+        'b': np.arange(3)
+    }
 
+    # Uncomment `backend` for parallel execution
     ys = th.util.map(
-        square,
-        xs
+        multiply,
+        xs,
+        # backend='multiprocessing',
     )
 
     print(ys)
-
 
 
 if __name__ == "__main__":
