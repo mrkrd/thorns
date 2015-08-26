205aef95f4486842f8ff9de7bbc80cfd64515e97
Marek Rudnicki <marekrud@posteo.de>
2015-08-26 13:54:51 +0200

    fix: move IPython import back (ups)

--- thorns/util/maps.py
+++ thorns/util/maps.py
@@ -172,8 +172,9 @@ def _multiprocessing_map(func, iterable, cfg):
 
 def _ipython_map(func, iterable, cfg):
 
+    import IPython
+
     if int(IPython.__version__.split('.')[0]) < 4:
-        import IPython
         from IPython.parallel import Client
     else:
         from ipyparallel import Client
