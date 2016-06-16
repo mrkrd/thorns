4bd48dc5057686d4371b3c2dd23e311a90b77a3a
mr <>
2016-06-16 19:01:16 +0200

    example: plot_spikes cleanup

--- examples/plot_spikes.py
+++ examples/plot_spikes.py
@@ -8,25 +8,20 @@ Zilany et al. (2014) model.
 
 from __future__ import division, absolute_import, print_function
 
-import matplotlib.pyplot as plt
-import os
-
 import thorns as th
 from thorns.datasets import load_anf_zilany2014
 
 
 def main():
 
-    ### Load spike trains
+    # Load spike trains
     spike_trains = load_anf_zilany2014()
 
     print(spike_trains.head())
 
-
-
-    ### Calculate vector strength
+    # Calculate vector strength
     cf, = spike_trains.cf.unique()
-    onset = 10e-3               # ms
+    onset = 10e-3
 
     trimmed = th.trim(spike_trains, onset, None)
     vs = th.vector_strength(trimmed, freq=cf)
@@ -34,15 +29,12 @@ def main():
     print()
     print("Vector strength: {}".format(vs))
 
-
-
-    ### Plot raster plot
+    # Plot raster plot
     th.plot_raster(spike_trains)
 
-
-
-    ### Show the plot
+    # Show the plot
     th.show()                   # Equivalent to plt.show()
+
 
 if __name__ == "__main__":
     main()
